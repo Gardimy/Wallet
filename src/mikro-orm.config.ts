1bdfd4034074badb407dc1c91ef686f3051ad4c6
Gardimy <gardimycharles889@gmail.com>
2025-06-05 11:50:52 -0400

    Update src/mikro-orm.config.ts file

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,17 +1,20 @@
-import { Options } from '@mikro-orm/core';
+// mikro-orm.config.ts
+
+import { MikroOrmModuleOptions } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Wallet } from './entities/wallet.entity';
 import { WalletOwner } from './entities/wallet-owner.entity';
 import { LedgerAccount } from './entities/ledger-account.entity';
 import { Transaction } from './entities/transaction.entity';
 
-const config: Options = {
-  type: 'mysql',
+const config: MikroOrmModuleOptions = {
+  driver: PostgreSqlDriver,
   host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'votre_mot_de_passe',
-  dbName: 'haitipay',
-  entities: [WalletOwner, Wallet, LedgerAccount, Transaction], // ✅ Still fine here
+  port: 5432,
+  user: 'postgres',
+  password: 'gardy', // replace with your real password
+  dbName: 'haiti_pay',
+  entities: [WalletOwner, Wallet, LedgerAccount, Transaction],
   debug: true,
 };
 
